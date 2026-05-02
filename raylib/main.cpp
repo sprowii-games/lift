945b0e9ff9143fdad5b04bf9a2936989356d0968
sprouee <sprouee@ro.ru>
2026-05-03 01:04:49 +0200

    а нет, ничего я не добавил

--- raylib/main.cpp
+++ raylib/main.cpp
@@ -6,21 +6,24 @@
 int main() {
     const int monitorWidth = GetMonitorWidth(GetCurrentMonitor());
     const int monitorHeight = GetMonitorHeight(GetCurrentMonitor());
-
+    
     // Set the flag
     SetConfigFlags(FLAG_FULLSCREEN_MODE);
     InitWindow(monitorWidth, monitorHeight, "LIFT");
     SetTargetFPS(60);
+    Texture2D door_opened = LoadTexture("textures/elevator_opened.png");
     Player player;
     Cam camera;
     Menu menu;
-    //Font myfont = LoadFont("")
+    //Font myfont = LoadFont("");
     camera.Init();
     menu.Init();
+    bool islevelcompleted = false;
     const int width = GetScreenWidth();
     const int height = GetScreenHeight();
     Vector2 rectangle = { width / 2, height / 2 };
     Color rect = RED;
+    short level = -4;
     bool is_puzzle_active = false;
     float camera_speed = 5.0f;
     player.pos = { (float)width / 100.0f * 16.6f,(float)height / 100.0f * 75 };
@@ -33,6 +36,10 @@ int main() {
             EndDrawing();
         }
         else if (menu.state == PLAYING) {
+            if (islevelcompleted) {
+                level++;
+                islevelcompleted = false;
+            }
             player.Update();
 
             if (player.is_in_area(rectangle.x)) {
@@ -49,7 +56,7 @@ int main() {
 
             BeginMode2D(camera.cam);
             DrawRectangle(rectangle.x, rectangle.y, 50, 50, rect);
-
+            DrawTexture(door_opened, (float)width / 100.0f * 5.0f,(float)height / 100.0f * 50.0f, WHITE);
             player.Draw();
             EndMode2D();
             int x = (width / 2) - (800 / 2);
@@ -58,14 +65,15 @@ int main() {
             if (is_puzzle_active) {
                 DrawRectangle(x, y, 800, 600, BLACK);
             }
-            DrawText("there are two chairs...", x +3,y-2, 33, RAYWHITE);
+            
+            DrawText("there are two chairs...", x + 3, y - 2, 33, RAYWHITE);
             EndDrawing();
         }
         else if (menu.state == EXIT) {
             break; // выходим из цикла
         }
     }
-
+    UnloadTexture(door_opened);
     CloseWindow();
     return 0;
 }
